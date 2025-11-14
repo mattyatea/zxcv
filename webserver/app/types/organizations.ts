6001e882d1e2eed3e954543da65864a8db3a6adb
mattyatea <mattyacocacora0@gmail.com>
2025-11-14 11:12:04 +0900

    いい感じに

--- webserver/app/types/organizations.ts
+++
@@ -1,6 +0,0 @@
-import type { InferContractRouterOutputs } from "@orpc/contract";
-import type { contract } from "~/server/orpc/contracts";
-
-type Outputs = InferContractRouterOutputs<typeof contract>["organizations"]; // FIXME: そのうちちゃんと定義する場所を統一して、いい感じに使うようにする
-
-export type OrganizationPublicProfile = Outputs["getPublicProfile"];
